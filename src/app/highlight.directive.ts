a7dd7f08572577f354aa42610b501fc43d8dd239
albright.human <sungutialbright@gmail.com>
2022-04-11 06:54:40 +0300

    add elementref to access DOM element and style

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -3,10 +3,10 @@ import { Directive , ElementRef} from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
-// export class HighlightDirective {
+export class HighlightDirective {
 
-//   constructor( private elem: ElementRef) { 
-//     elem.nativeElement.style.backgroundColor ="yellow"
-//   }
+  constructor( private elem: ElementRef) { 
+    elem.nativeElement.style.backgroundColor ="yellow"
+  }
 
-// }
+}
