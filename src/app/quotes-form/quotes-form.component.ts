56b39c95ebcf50b7d6bcfbae9774fbca48e05273
albright.human <sungutialbright@gmail.com>
2022-04-11 06:33:02 +0300

    import class quote to form component

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit , Output, EventEmitter} from '@angular/core';
 import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quotes-form',
-//   templateUrl: './quotes-form.component.html',
-//   styleUrls: ['./quotes-form.component.css']
-// })
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
 // export class QuotesFormComponent implements OnInit {
 
 //   newQuote = new Quote("",0,"","",0,0);
