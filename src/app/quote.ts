e1df5cbb9269e742af03c23e8fc3eca7d359c1ed
albright.human <sungutialbright@gmail.com>
2022-04-11 06:25:42 +0300

    create quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,21 +1,21 @@
-// export class Quote {
-//     name: string;
-//     id: number;
-//     quote:string;
-//     author:string;
-//     upvotes:number;
-//     downvotes:number;
+export class Quote {
+    name: string;
+    id: number;
+    quote:string;
+    author:string;
+    upvotes:number;
+    downvotes:number;
     
-// constructor(name: string, id: number, quote:string, author:string,upvotes:number,downvotes:number) {
+constructor(name: string, id: number, quote:string, author:string,upvotes:number,downvotes:number) {
    
-//     this.id = id;
-//     this.name = name;
-//     this.quote = quote;
-//     this.author = author;
-//     this.upvotes = upvotes;
-//     this.downvotes = downvotes;
+    this.id = id;
+    this.name = name;
+    this.quote = quote;
+    this.author = author;
+    this.upvotes = upvotes;
+    this.downvotes = downvotes;
     
-// }
+}
 
 
 
