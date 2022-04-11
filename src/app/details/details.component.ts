759d68e135fdb603bcd4c722a525a99f28da2d5d
albright.human <sungutialbright@gmail.com>
2022-04-11 06:45:59 +0300

    add quote details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,15 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-details',
-//   templateUrl: './details.component.html',
-//   styleUrls: ['./details.component.css']
-// })
-// export class DetailsComponent implements OnInit {
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
